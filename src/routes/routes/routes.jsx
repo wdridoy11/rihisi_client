7012e1204bb8ede07ed39ee36264d79a0ab2bc3a
Ridoy Sharif <developersridoy@gmail.com>
2023-10-21 19:49:52 +0600

    rihisi client code

---
+++ src/routes/routes/routes.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {createBrowserRouter} from "react-router-dom";
+import Main from '../../layout/Main';
+import Home from '../../pages/Home/Home';
+// page import
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Main></Main> ,
+      children:[
+        {
+            path:"/",
+            element:<Home></Home>
+        }
+      ],
+    },
+    
+  ]);
+
+
+export default router
